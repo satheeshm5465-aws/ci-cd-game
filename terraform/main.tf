578a9e07ed5584134e1eee8c0b65a6f04a5c647a
satheesh <satheesh.m5465@gmail.com>
2026-02-13 10:49:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "game_server" {
   instance_type = "t3.micro"
   key_name      = "jenkins"
 
-  security_groups = [aws_security_group.game_sg_jenkins.name]
+  security_groups = [aws_security_group.gamesathe.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "game_sg_jenkins" {
-  name = "game_sg_jenkins"
+resource "aws_security_group" "gamesathe" {
+  name = "gamesathe"
 
   ingress {
     from_port   = 22
